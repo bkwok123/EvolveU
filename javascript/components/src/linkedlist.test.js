8c3025cd92f23d0cdbc14c36e4ffac1dc6e4ffc7
Boris Kwok <bkwok123@gmail.com>
2020-04-24 12:53:35 -0600

    Insert (to tail) added

--- javascript/components/src/linkedlist.test.js
+++ javascript/components/src/linkedlist.test.js
@@ -3,11 +3,11 @@ import Linked from './linkedlist.js';
 // Reference: https://jestjs.io/docs/en/using-matchers
 test('Check the ListNode class instantiation', () => {
 
-    const list = new Linked.ListNode("Test",10);
-    expect(list.forwardNode).toBe(null);
-    expect(list.backwardNode).toBe(null);
-    expect(list.subject).toBe("Test");
-    expect(list.amount).toBe(10);
+    const node = new Linked.ListNode("Test",10);
+    expect(node.forwardNode).toBe(null);
+    expect(node.backwardNode).toBe(null);
+    expect(node.subject).toBe("Test");
+    expect(node.amount).toBe(10);
 });
 
 test('Check the LinkedList class instantiation', () => {
@@ -15,15 +15,62 @@ test('Check the LinkedList class instantiation', () => {
     const list = new Linked.LinkedList();
     expect(list.head).toBe(null);
     expect(list.tail).toBe(null);
+    expect(list.current).toBe(null);
 });
 
+test('Check the ListNode class insert method', () => {
 
-// test('Check the ListNode class deposit method', () => {
+    const list = new Linked.LinkedList();
+    let currentNode = list.insert("Test",10);
+    expect(list.head).not.toBe(null);
+    expect(list.head.subject).toBe("Test");
+    expect(list.head.amount).toBe(10);
+    expect(list.head).toEqual(currentNode);
+    expect(list.tail).toEqual(currentNode);
 
-//     const account = new Account("Saving",20)
+    currentNode = list.head;
+    list.insert("Test2",20);
+    currentNode = currentNode.forwardNode;
+    expect(currentNode.subject).toBe("Test2");
+    expect(currentNode.amount).toBe(20);
+    expect(currentNode.backwardNode).toEqual(list.head);    
+    expect(list.head.forwardNode).toEqual(currentNode);
+    expect(list.tail).toEqual(currentNode);    
 
-//     expect(account.deposit(10)).toBe(30);
-//     console.log(account.startingBalance);
-//     expect(account.deposit(50)).toBe(80);
-//     console.log(account.startingBalance);
-// });
+    let prevnode = currentNode;       
+    currentNode = list.insert("Test3",30);
+    expect(currentNode.subject).toBe("Test3");
+    expect(currentNode.amount).toBe(30);
+    expect(currentNode.backwardNode).toEqual(prevnode);    
+    expect(prevnode.forwardNode).toEqual(currentNode);        
+    expect(list.tail).toEqual(currentNode);
+});
+
+test('Check the ListNode class first method', () => {
+
+    const list = new Linked.LinkedList();
+    list.insert("Test",10);
+    let node = list.first();
+    expect(node.subject).toBe("Test");
+    expect(node.amount).toBe(10);
+
+    list.insert("Test2",20);
+    node = list.first();
+    expect(node.subject).toBe("Test");
+    expect(node.amount).toBe(10);   
+});
+
+test('Check the ListNode class last method', () => {
+
+    const list = new Linked.LinkedList();
+    list.insert("Test",10);
+    list.insert("Test2",20);
+    let node = list.last();
+    expect(node.subject).toBe("Test2");
+    expect(node.amount).toBe(20);
+
+    list.insert("Test3",30);
+    node = list.last();
+    expect(node.subject).toBe("Test3");
+    expect(node.amount).toBe(30);   
+});
