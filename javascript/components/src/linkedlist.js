1a6270218caeb5b65838ad90b3c07c3e7954cf0f
Boris Kwok <bkwok123@gmail.com>
2020-04-23 22:12:02 -0600

    Linked List Draft

---
+++ javascript/components/src/linkedlist.js
@@ -0,0 +1,55 @@
+class ListNode {
+    constructor() {
+        this.forwardNode = ;
+        this.backwardNode = null;
+        this.subject = null;
+        this.amount = null;
+    }    
+    
+    // Show the subject and the amount
+    show() {
+
+    }
+
+    // Position to the first node
+    first() {
+
+    }
+
+    // Position to the last node
+    last() {
+
+    }
+    
+    // Move to the next node
+    next() {
+
+    }
+
+    // Backup one node
+    previous() {
+
+    }
+
+    // Inserts a new node after the current node (which node will be the current node after the insertion?)
+    insert() {
+
+    }
+
+    // Delete the current node (which node will be the current node after the deletion?)
+    delete() {
+
+    }
+
+    // Show the total of all the amounts of all the ListNodes
+    showtotal() {
+
+    }
+
+    // ●	Allow the user to sort by subject
+    // ●	Allow the user to sort by amount    
+}
+
+class LinkedList {
+
+}
